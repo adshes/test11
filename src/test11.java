74c203ed038ec05a9bcf044deb5cd7280b2a6520
adshes <42263959+adshes@users.noreply.github.com>
2019-12-22 21:26:51 +0300

    Initial commit

---
+++ src/test11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class test11 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String ct = in.nextLine();
+
+        System.out.println(ct);
+    }
+
+}
